c02b10d521c9702b537a2ecc0fd63c82e2159b6a
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-07-23 22:19:36 +0100

    updating modelingToolkit example
    now runs on v9.26.0
    seems to give a different bifurcation plot
    not sure why yet
    indexing in ModelingToolkit v9 gets a bit awkward

--- modelingToolkit.jl
+++ modelingToolkit.jl
@@ -1,34 +1,51 @@
 using Revise, ModelingToolkit, LinearAlgebra
+using ModelingToolkit: t_nounits as t, D_nounits as D
 using DifferentialEquations, Plots
 using BifurcationKit
 const BK = BifurcationKit
 
 indexof(sym, syms) = findfirst(isequal(sym),syms)
 
-@variables t E(t) x(t) u(t) SS0(t) SS1(t) 	# independent and dependent variables
-@parameters U0 τ J E0 τD U0 τF τS α    		# parameters
-D = Differential(t) 				# define an operator for the differentiation w.r.t. time
+@mtkmodel NMMODEL begin
+    @parameters begin
+        U0 = 0.3
+        τ = 0.013
+        J = 3.07
+        E0 = -2.0
+        τD = 0.200
+        τF = 1.5
+        τS = 0.007
+        α = 1.5
+    end
+    @variables begin
+        E(t) = 0.238616
+        x(t) = 0.982747
+        u(t) = 0.367876
+        SS0(t)
+        SS1(t)
+    end
+    @equations begin
+        SS0 ~ J * u * x * E + E0
+        SS1 ~ α * log(1 + exp(SS0 / α))
+        D(E) ~ (-E + SS1) / τ
+        D(x) ~ (1.0 - x) / τD - u * x * E
+        D(u) ~ (U0 - u) / τF +  U0 * (1 - u) * E
+    end
+end
 
-# define the model
-@named NMmodel = ODESystem([SS0 ~ J * u * x * E + E0,
-	SS1 ~ α * log(1 + exp(SS0 / α)),
-	D(E) ~ (-E + SS1) / τ,
-	D(x) ~ (1.0 - x) / τD - u * x * E,
-	D(u) ~ (U0 - u) / τF +  U0 * (1 - u) * E],
-	defaults = Dict(E => 0.238616, x => 0.982747, u => 0.367876,
-	α => 1.5, τ => 0.013, J => 3.07, E0 => -2.0, τD => 0.200, U0 => 0.3, τF => 1.5, τS => 0.007))
+@mtkbuild NMmodel = NMMODEL()
 
 # get the vector field and jacobian
-odeprob = ODEProblem(structural_simplify(NMmodel), [], (0.,10.), [], jac = true)
+odeprob = ODEProblem(NMmodel, [], (0.,10.), [], jac = true)
 odefun = odeprob.f
 F = (u,p) -> odefun(u,p,0)
 J = (u,p) -> odefun.jac(u,p,0)
 
-id_E0 = indexof(E0, parameters(NMmodel))
-par_tm = odeprob.p
-
+id_E0 = indexof(NMmodel.E0, parameters(NMmodel))
+par_tm = [NMmodel.U0 => 0.3, NMmodel.τ => 0.013, NMmodel.J => 3.07, NMmodel.E0 => -2.0, NMmodel.τD => 0.200, NMmodel.τF => 1.5, NMmodel.τS => 0.007, NMmodel.α => 1.5]
+p = ModelingToolkit.varmap_to_vars(par_tm, parameters(NMmodel))
 # we collect the differentials together in a problem
-prob = BifurcationProblem(F, odeprob.u0, par_tm, (@lens _[id_E0]); J = J,
+prob = BifurcationProblem(F, odeprob.u0, p, (@lens _[id_E0]); J = J,
     record_from_solution = (x, p) -> (E = x[1], x = x[2], u = x[3]))
 
 # continuation options
@@ -44,3 +61,6 @@ opts_br = ContinuationPar(p_min = -10.0, p_max = -0.9,
 br = continuation(prob, PALC(tangent = Bordered()), opts_br; normC = norminf)
 
 scene = plot(br, plotfold=false, markersize=3, legend=:topleft)
+# Plot 9 is the correct one, plot 10 could be wrong because:
+# 1. The parameters or initial conditions are in the wrong order
+# 2. The output variables in record from solution are wrong
