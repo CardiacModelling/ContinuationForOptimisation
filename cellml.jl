3f2e9dfd62c67199b4395058e841dd32be08bf78
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-08-07 11:31:45 +0100

    bit closer on cellml bifurcation

--- cellml.jl
+++ cellml.jl
@@ -36,8 +36,28 @@ display(title!("Norm of conc for long run time"))
 
 # Check converged by getting 1 pulse
 prob = remake(prob; tspan=(0, 10000.0), u0=sol[end])
-sol = solve(prob, Rodas5(), dtmax=1.)
-concs = (sol[idx_concs,:]./sol[idx_concs,1])
-plot(sol.t, concs')
+sol_pulse = solve(prob, Rodas5(), dtmax=1.)
+concs = (sol_pulse[idx_concs,:]./sol_pulse[idx_concs,1])
+plot(sol_pulse.t, concs')
 display(title!("Normalised concentrations at limit cycle"))
 
+# Create bifurcation problem and look at limit cycle continuation
+# Convert states and parameters to Num
+function convert_to_num(arr)
+	return [convert(Num, pi[1]) => pi[2] for pi in arr]
+end
+
+bp = BifurcationProblem(ml.sys, convert_to_num(list_states(ml)), convert_to_num(list_params(ml)), ml.sys.rapid_time_dependent_potassium_current.g_Kr)
+
+bptrap, ci = BK.generate_ci_problem(PeriodicOrbitTrapProblem(M = 150),
+bp, sol_pulse, 1000.)
+
+opts_cp = ContinuationPar(p_min = 0.04, p_max = 1.5,
+# parameters to have a smooth continuation curve
+dsmin = 0.0001, dsmax = 0.01,
+max_steps = 50, tol_stability = 1e-8,
+)
+
+brpo_fold = continuation(bptrap, ci, PALC(), opts_cp;
+	verbosity = 3, plot = true,
+)
