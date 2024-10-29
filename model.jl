ab61076aeab070af6eeda602e8e2209134429079
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-10-29 13:14:51 +0000

    updated model.jl to move to noble model

--- model.jl
+++ model.jl
@@ -5,82 +5,82 @@ using BifurcationKit
 
 export ode!, params, ic, ic_conv, slow_idx, plot_idx, cont_params
 
-function ode!(dz, z, p, t=0)
-    am1, am2, am3, bm1, bm2, bm3, ah1, ah2, ah3, bh1, bh2, Ena, an1, an2, an3, bn1, bn2, bn3, 
-    Ek, as1, as2, as3, bs1, bs2, bs3, Es, El, Cm = 0.1, 50.0, 10.0, 4.0, 75.0, 18.0, 0.07, 75.0, 
-    20.0, 45.0, 10.0, 40.0, 0.01, 65.0, 10.0, 0.125, 75.0, 80.0, -87.0, 1e-6, 65.0, 10.0, 1e-4, 
-    75.0, 80.0, -87.0, -64.387, 1.0
+function noble!(dz, z, p, t=0)
+	@unpack g_Na_sf, g_K_sf, g_L_sf = p
 
-	@unpack gna, gk, gs, gl = p
+	V, m, h, n = z
+	
+	alpha_m = 100*(-V-48)/(exp((-V-48)/15)-1)
+	beta_m = 120*(V+8)/(exp((V+8)/5)-1)
+	alpha_h = 170*exp((-V-90)/20)
+	beta_h = 1000/(1+exp((-V-42)/10))
+	alpha_n = 0.1*(-V-50)/(exp((-V-50)/10)-1)
+	beta_n = 2*exp((-V-90)/80)
 
-	m, h, n, s, V = z
+	g_Na = g_Na_sf*m^3*h*400000
+	g_K1 = g_K_sf*1200*exp((-V-90)/50)+15*exp((V+90)/60)
+	g_K2 = g_K_sf*1200*n^4
 
-    alpha_m=-am1*(V+am2)/(exp(-(V+am2)/am3)-1)
-    beta_m=bm1*exp(-(V+bm2)/bm3)
-    alpha_h=ah1*exp(-(V+ah2)/ah3)
-    beta_h=1/(exp(-(V+bh1)/bh2)+1)
-    alpha_n=-an1*(V+an2)/(exp(-(V+an2)/an3)-1)
-    beta_n=bn1*exp((V+bn2)/bn3)
-	alpha_s=-as1*(V+as2)/(exp(-(V+as2)/as3)-1)
-	beta_s=bs1*exp((V+bs2)/bs3)
-    
-	dz[1] = alpha_m*(1-m)-beta_m*m
-	dz[2] =	alpha_h*(1-h)-beta_h*h
-	dz[3] = alpha_n*(1-n)-beta_n*n
-	dz[4] = alpha_s*(1-s)-beta_s*s
-    dz[5] = -(gna*m^3*h*(V-Ena)+gk*n^4*(V-Ek)+gs*s^4*(V-Es)+gl*(V-El))/Cm
-    dz
+	i_Leak = g_L_sf*75*(V+60)
+	i_Na = (g_Na+140)*(V-40)
+	i_K = (g_K1+g_K2)*(V+100)
+
+	dz[1] = -(i_Na+i_K+i_Leak)/12
+	dz[2] = alpha_m*(1-m)-beta_m*m
+	dz[3] = alpha_h*(1-h)-beta_h*h
+	dz[4] = alpha_n*(1-n)-beta_n*n
+
+	dz
 end
 
-function ode_cont!(dz, z, p, t=0)
-    am1, am2, am3, bm1, bm2, bm3, ah1, ah2, ah3, bh1, bh2, Ena, an1, an2, an3, bn1, bn2, bn3, 
-    Ek, as1, as2, as3, bs1, bs2, bs3, Es, El, Cm = 0.1, 50.0, 10.0, 4.0, 75.0, 18.0, 0.07, 75.0, 
-    20.0, 45.0, 10.0, 40.0, 0.01, 65.0, 10.0, 0.125, 75.0, 80.0, -87.0, 1e-6, 65.0, 10.0, 1e-4, 
-    75.0, 80.0, -87.0, -64.387, 1.0
+function noble_cont!(dz, z, p, t=0)
+	@unpack g_Na_sf, g_K_sf, g_L_sf, na_step, k_step, l_step, step = p
 
-	@unpack gna, gk, gs, gl, gna_step, gk_step, gs_step, gl_step, step = p
+	V, m, h, n = z
+	
+    g_Na_sf = g_Na_sf + na_step*step
+    g_K_sf = g_K_sf + k_step*step
+    g_L_sf = g_L_sf + l_step*step
 
-    gna = gna + gna_step*step
-    gk = gk + gk_step*step
-    gs = gs + gs_step*step
-    gl = gl + gl_step*step
+	alpha_m = 100*(-V-48)/(exp((-V-48)/15)-1)
+	beta_m = 120*(V+8)/(exp((V+8)/5)-1)
+	alpha_h = 170*exp((-V-90)/20)
+	beta_h = 1000/(1+exp((-V-42)/10))
+	alpha_n = 0.1*(-V-50)/(exp((-V-50)/10)-1)
+	beta_n = 2*exp((-V-90)/80)
 
-	m, h, n, s, V = z
+	g_Na = g_Na_sf*m^3*h*400000
+	g_K1 = g_K_sf*1200*exp((-V-90)/50)+15*exp((V+90)/60)
+	g_K2 = g_K_sf*1200*n^4
 
-    alpha_m=-am1*(V+am2)/(exp(-(V+am2)/am3)-1)
-    beta_m=bm1*exp(-(V+bm2)/bm3)
-    alpha_h=ah1*exp(-(V+ah2)/ah3)
-    beta_h=1/(exp(-(V+bh1)/bh2)+1)
-    alpha_n=-an1*(V+an2)/(exp(-(V+an2)/an3)-1)
-    beta_n=bn1*exp((V+bn2)/bn3)
-	alpha_s=-as1*(V+as2)/(exp(-(V+as2)/as3)-1)
-	beta_s=bs1*exp((V+bs2)/bs3)
-    
-	dz[1] = alpha_m*(1-m)-beta_m*m
-	dz[2] =	alpha_h*(1-h)-beta_h*h
-	dz[3] = alpha_n*(1-n)-beta_n*n
-	dz[4] = alpha_s*(1-s)-beta_s*s
-    dz[5] = -(gna*m^3*h*(V-Ena)+gk*n^4*(V-Ek)+gs*s^4*(V-Es)+gl*(V-El))/Cm
-    dz
+	i_Leak = 75*(V+60)
+	i_Na = (g_Na+140)*(V-40)
+	i_K = (g_K1+g_K2)*(V+100)
+
+	dz[1] = -(i_Na+i_K+i_Leak)/12
+	dz[2] = alpha_m*(1-m)-beta_m*m
+	dz[3] = alpha_h*(1-h)-beta_h*h
+	dz[4] = alpha_n*(1-n)-beta_n*n
+
+	dz
 end
 
 # parameter values
-params = (gna=120.0, gk=13.0, gs=10.0, gl=0.3)::NamedTuple{(:gna, :gk, :gs, :gl), Tuple{Float64, Float64, Float64, Float64}}
-params_cont = (gna=120.0, gk=13.0, gs=10.0, gl=0.3, gna_step=0.0, gk_step=0.0, gs_step=0.0, gl_step=0.0, step=0.0)::NamedTuple{(:gna, :gk, :gs, :gl, :gna_step, :gk_step, :gs_step, :gl_step, :step), Tuple{Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64, Float64}}
+params = (g_Na_sf=1.0, g_K_sf=1.0, g_L_sf=1.0)::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf), Tuple{Float64, Float64, Float64}}
+params_cont = (g_Na_sf=1.0, g_K_sf=1.0, g_L_sf=1.0, na_step=0.0, k_step=0.0, l_step=0.0, step=0.0)::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf, :na_step, :k_step, :l_step, :step), Tuple{Float64, Float64, Float64, Float64, Float64, Float64, Float64}}
 
 # initial condition
-ic = [0.05, 0.6, 0.325, 0.3, -75.0]
+ic = [-87.0, 0.01, 0.8, 0.01]
 
-# Converged initial conditions - 50,000ms at (abs=1e-10, rel=1e-8) tolerances
-ic_conv = [0.13752161502545546,
-0.4393685543525413,
-0.3638744137880853,
-0.12343178944696619,
--64.37702302658744,
-]
+# Converged initial conditions - 200s at (abs=1e-10, rel=1e-10) tolerances
+ic_conv = [ -8.937179276718057,
+0.8653023029331617,
+0.0030371533410818727,
+0.6155744987359527,
+] #update at end
 
-slow_idx = 4
+slow_idx = 2
 
-plot_idx = 5
+plot_idx = 1
 
 end
