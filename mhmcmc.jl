55f31c31f6fa4a409731eaa50eacab89fe160494
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-11-04 16:05:14 +0000

    mcmc time units for noble
    noble is given in seconds not milliseconds

--- mhmcmc.jl
+++ mhmcmc.jl
@@ -395,7 +395,7 @@ Get the period of the limit cycle.
 """
 function get_period(lc::Vector{Float64}, prob::ODEProblem)::Number
     # Long simulation
-    sol = DifferentialEquations.solve(prob, Tsit5(), tspan=(0.0, 50000.0), u0=lc)::ODESolution
+    sol = DifferentialEquations.solve(prob, Tsit5(), tspan=(0.0, 500.0), u0=lc)::ODESolution
     # Get local maximums
     maxs = []
     for i in 2:length(sol.t)-1
