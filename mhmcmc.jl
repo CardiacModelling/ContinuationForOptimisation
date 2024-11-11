43908c78100ab546e6c2e2c75e06fdd2d7a26195
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-11-11 11:42:44 +0000

    moved some general functions to tools.jl

--- mhmcmc.jl
+++ mhmcmc.jl
@@ -255,36 +255,6 @@ function ll(limitCycle::Vector{Float64}, data::Vector{Float64}, σ::Number, prob
 end
 
 """
-    aligned_sol(lc::Vector{Float64}, prob::ODEProblem, period::Number = 0.0; save_only_V::Bool = true)
-
-Align the limit cycle in the solution to start at the max of V and fixes the timesteps for recording the data.
-
-# Arguments
-- `lc::Vector{Float64}`: The limit cycle to align.
-- `prob::ODEProblem`: The ODEProblem for the model.
-- `period::Number`: The period of the data.
-- `save_only_V::Bool=true`: Save only the V (voltage) variable.
-
-# Returns
-- `sol::ODESolution`: The aligned solution.
-- `period::Number`: The period of the limit cycle.
-"""
-function aligned_sol(lc::Vector{Float64}, prob::ODEProblem, period::Number; save_only_V::Bool = true)
-    # Simulation of length 2*period to find the max of V
-    sol = DifferentialEquations.solve(prob, Tsit5(); tspan=(0.0, period*2.0), u0=lc, save_idxs=Model.plot_idx, saveat=1e-5, dense=false, maxiters=1e9)::ODESolution
-    # Find the time where V is maximised
-    t = sol.t[argmax(sol.u)]
-    # Find the states at that time
-    sol = DifferentialEquations.solve(prob, Tsit5(); tspan = (0.0,t), u0=sol.prob.u0, save_everystep=false, save_start=false, maxiters=1e9)
-    # Get the aligned solution
-    if save_only_V
-        return DifferentialEquations.solve(prob, Tsit5(), saveat=0.01, save_idxs=Model.plot_idx, tspan=(0.0, period), u0=sol.u[end], maxiters=1e9)::ODESolution, period
-    else
-        return DifferentialEquations.solve(prob, Tsit5(), saveat=0.01, tspan=(0.0, period), u0=sol.u[end], maxiters=1e9)::ODESolution, period
-    end
-end
-
-"""
     odeSolverFull(x::Vector{Float64}, prob::ODEProblem, lc::Vector{Float64}, xlc::Vector{Float64}, paramMap::Function, verbose::Integer)::Vector{Float64}
 
 Solve the ODE until convergence starting from the default initial conditions.
@@ -394,82 +364,7 @@ function contSolver(x::Vector{Float64}, prob::ODEProblem, lc::Vector{Float64}, x
     end
 end
 
-"""
-    get_period(lc::Vector{Float64}, prob::ODEProblem)::Number
-
-Get the period of the limit cycle.
-
-# Arguments
-- `lc::Vector{Float64}`: The limit cycle.
-- `prob::ODEProblem`: The ODEProblem for the model.
-
-# Returns
-- `period::Number`: The period of the limit cycle.
-"""
-function get_period(lc::Vector{Float64}, prob::ODEProblem)::Number
-    # Long simulation
-    sol = DifferentialEquations.solve(prob, Tsit5(), tspan=(0.0, 100.0), u0=lc, maxiters=1e9)::ODESolution
-    # Get local maximums
-    maxs = []
-    for i in 2:length(sol.t)-1
-        if sol[1, i] > sol[1, i+1] && sol[1, i] > sol[1, i-1]
-            push!(maxs, i)
-        end
-    end
-    # Get average period (time between maxs)
-    pulse_widths = [sol.t[maxs[i]]-sol.t[maxs[i-1]] for i in 2:length(maxs)]
-    period = mean(pulse_widths)
-    return period
-end
-
-"""
-    param_map(x::Vector{Float64})::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf), Tuple{Number, Number, Number}}
-
-Map the parameters from a `Vector` to a `NamedTuple`.
-
-# Arguments
-- `x::Vector{Float64}`: The parameters in a `Vector` ordered as gna, gk, gl. The noise parameter can be optionally included at the end.
-
-# Returns
-- `par::NamedTuple`: The parameters as a `NamedTuple`.
-"""
-function param_map(x::Vector{Float64})::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf), Tuple{Number, Number, Number}}
-    # Load global const parameters p
-    par = p
-    # Set the parameters from the state x
-    par = @set par.g_Na_sf = x[1]
-    par = @set par.g_K_sf = x[2]
-    par = @set par.g_L_sf = x[3]
-    return par
-end
-
-"""
-    param_map(x::Vector{Float64}, xlc::Vector{Float64})::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf, :na_step, :k_step, :l_step, :step), Tuple{Number, Number, Number, Number, Number, Number, Number}}
-
-Map the parameters from a `Vector` to a `NamedTuple`.
-
-Specific to the continuation solver.
-
-# Arguments
-- `x::Vector{Float64}`: The parameters to find the limit cycle for.
-- `xlc::Vector{Float64}`: The parameters of the previous limit cycle.
-
-# Returns
-- `par::NamedTuple`: The parameters as a `NamedTuple`.
-"""
-function param_map(x::Vector{Float64}, xlc::Vector{Float64})::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf, :na_step, :k_step, :l_step, :step), Tuple{Number, Number, Number, Number, Number, Number, Number}}
-    # Load global const parameters p
-    par = p
-    # Set the parameters from the state xlc (starting location for continuation)
-    par = @set par.g_Na_sf = xlc[1]
-    par = @set par.g_K_sf = xlc[2]
-    par = @set par.g_L_sf = xlc[3]
-    # Set the continuation step sizes
-    par = @set par.na_step = x[1] - xlc[1]
-    par = @set par.k_step = x[2] - xlc[2]
-    par = @set par.l_step = x[3] - xlc[3]
-    return par
-end
+aligned_sol = Tools.aligned_sol
 
 # Method selection and settings
 const use_continuation = true
--- tools.jl
+++ tools.jl
@@ -1,28 +1,13 @@
 module Tools
-using Distributions, DifferentialEquations, Plots
+using Distributions, DifferentialEquations, Plots, Accessors
 
-function convergence_plot(sol, dt=1000, u=0)
-    if u==0
-        u = 1:length(sol.u[1])
-    end
-    println(u)
-    # Plot the change in the states across each pulse
-    error = []
-    for i in dt:dt:sol.t[end]
-        push!(error, norm(((sol(i)-sol(i-dt))./scaling)[u]))
-    end
-    println(error)
-    # Plot error on a log scale
-    plot(1:length(error), error, yscale=:log10)
-    title!("Convergence plot: " * string(u))
-    xlabel!("Pulse count")
-    display(ylabel!("Error"))
-end
+include("./model.jl")
+using .Model
 
 # Check all variables are converged automatically
 function auto_converge_check(prob::ODEProblem, ic::Vector{Float64}, p::NamedTuple)::Bool
     # Find the average across the first 10s for each state
-    sol = solve(prob, Tsit5(), u0=ic, p=p, tspan=(0.0, 10.0), maxiters=1e9)
+    sol = solve(prob, Tsit5(), u0=ic, p=p, tspan=(0.0, 10.0), maxiters=1e9) # TODO I think this needs a save every 0.01s type of thing, same for end
     avgs = mean(sol.u)
     # Run for further 80s to try and converge closer
     sol = solve(prob, Tsit5(), u0=sol[end], p=p, tspan=(0.0, 80.0), save_everystep=false, save_start=false, maxiters=1e9)
@@ -31,6 +16,82 @@ function auto_converge_check(prob::ODEProblem, ic::Vector{Float64}, p::NamedTupl
     # If avgs is inside the range of the final 10s then it is converged
     sol = stack(sol.u)
     return all(minimum(sol, dims=2) .< avgs .< maximum(sol, dims=2))
+end
+
+"""
+    aligned_sol(lc, prob::ODEProblem, period::Number = 0.0; save_only_V::Bool = true)
+
+Align the limit cycle in the solution to start at the max of V and fixes the timesteps for recording the data.
+
+# Arguments
+- `lc`: The limit cycle to align.
+- `prob::ODEProblem`: The ODEProblem for the model.
+- `period::Number`: The period of the data.
+- `save_only_V::Bool=true`: Save only the V (voltage) variable.
+
+# Returns
+- `sol::ODESolution`: The aligned solution.
+- `period::Number`: The period of the limit cycle.
+"""
+function aligned_sol(lc, prob::ODEProblem, period::Number; save_only_V::Bool = true)
+    # Simulation of length 2*period to find the max of V
+    sol = DifferentialEquations.solve(prob, Tsit5(); tspan=(0.0, period*2.0), u0=lc, save_idxs=Model.plot_idx, saveat=1e-5, dense=false, maxiters=1e9)::ODESolution
+    # Find the time where V is maximised
+    t = sol.t[argmax(sol.u)]
+    # Find the states at that time
+    sol = DifferentialEquations.solve(prob, Tsit5(); tspan = (0.0,t), u0=sol.prob.u0, save_everystep=false, save_start=false, maxiters=1e9)
+    # Get the aligned solution
+    if save_only_V
+        return DifferentialEquations.solve(prob, Tsit5(), saveat=0.01, save_idxs=Model.plot_idx, tspan=(0.0, period), u0=sol.u[end], maxiters=1e9, save_end = false)::ODESolution, period
+    else
+        return DifferentialEquations.solve(prob, Tsit5(), saveat=0.01, tspan=(0.0, period), u0=sol.u[end], maxiters=1e9, save_end = false)::ODESolution, period
+    end
+end
+
+"""
+    param_map(x)::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf), Tuple{Number, Number, Number}}
+
+Map the parameters from a `Vector` to a `NamedTuple`.
+
+# Arguments
+- `x`: The parameters in a `Vector` ordered as gna, gk, gl. The noise parameter can be optionally included at the end.
+
+# Returns
+- `par::NamedTuple`: The parameters as a `NamedTuple`.
+"""
+function param_map(x)::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf), Tuple{Number, Number, Number}}
+    par = Model.params
+    # Set the parameters from the state x
+    par = @set par.g_Na_sf = x[1]
+    par = @set par.g_K_sf = x[2]
+    par = @set par.g_L_sf = x[3]
+    return par
+end
+
+"""
+    param_map(x, xlc)::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf, :na_step, :k_step, :l_step, :step), Tuple{Number, Number, Number, Number, Number, Number, Number}}
+
+Map the parameters from a `Vector` to a `NamedTuple`.
+
+Specific to the continuation solver.
+
+# Arguments
+- `x`: The parameters to find the limit cycle for.
+- `xlc`: The parameters of the previous limit cycle.
+
+# Returns
+- `par::NamedTuple`: The parameters as a `NamedTuple`.
+"""
+function param_map(x, xlc)::NamedTuple{(:g_Na_sf, :g_K_sf, :g_L_sf, :na_step, :k_step, :l_step, :step), Tuple{Number, Number, Number, Number, Number, Number, Number}}
+    # Set the parameters from the state xlc (starting location for continuation)
+    par = @set Model.params_cont.g_Na_sf = xlc[1]
+    par = @set par.g_K_sf = xlc[2]
+    par = @set par.g_L_sf = xlc[3]
+    # Set the continuation step sizes
+    par = @set par.na_step = x[1] - xlc[1]
+    par = @set par.k_step = x[2] - xlc[2]
+    par = @set par.l_step = x[3] - xlc[3]
+    return par
 end
 
 end
