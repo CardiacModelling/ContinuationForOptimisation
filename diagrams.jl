916a6370b60dc076df7f1a9e9b9188effc6a2320
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-12-10 15:02:22 +0000

    updated diagrams
    new figure sizes, fixed some legend issues

--- diagrams.jl
+++ diagrams.jl
@@ -1,6 +1,6 @@
 using Plots, LaTeXStrings, Distributions
 
-plotParams = (linewidth=2., dpi=300, size=(450,300))
+plotParams = (linewidth=2., dpi=300)
 l = @layout [a b]
 # Panel A
 initialCondition = 1.9
@@ -23,7 +23,7 @@ plot!(p1:0.01:p2, lowerLine, color=:blue, label="Continuation"; plotParams...)
 plot!([p2, p2], [upperLine(p2), initialCondition], label="", color=:red; plotParams...)
 
 # Initial condition
-plotA = hline!([initialCondition], label="Initial Condition", color=:pink, legend=false; plotParams...)
+plotA = hline!([initialCondition], label="IC", color=:pink, legend=false; plotParams...)
 
 # Panel B
 initialCondition = 0.53
@@ -44,9 +44,9 @@ plot(xCurve1(y), y, xlim=(0,1), ylim=(0,1), linestyle=:dot, color=:black, label=
 y = centery1:0.01:1
 plot(xCurve1(y), y, xlim=(0,1), ylim=(0,1), color=:black, label="", left_margin=5Plots.mm; plotParams...)
 y = 0:0.01:centery2
-plot!(xCurve2(y), y, color=:black, label="Stable Limit Cycle"; plotParams...)
+plot!(xCurve2(y), y, color=:black, label="Stable LC"; plotParams...)
 y = centery2:0.01:1
-plot!(xCurve2(y), y, color=:black, label="Unstable Limit Cycle", linestyle=:dot, xticks=([p1, p2], [L"p_1", L"p_2"]), yticks=nothing; plotParams...)
+plot!(xCurve2(y), y, color=:black, label="Unstable LC", linestyle=:dot, xticks=([p1, p2], [L"p_1", L"p_2"]), yticks=nothing; plotParams...)
 xlabel!("Parameter")
 ylabel!("State")
 
@@ -54,18 +54,18 @@ ylabel!("State")
 criticalY = centery1+sqrt((centerx1-p1)/curve1)
 plot!([p1, p1], [initialCondition, criticalY], label="", color=:blue; plotParams...)
 y = criticalY:-0.01:centery1
-plot!(xCurve1(y), y, color=:blue, label="Continuation Approach"; plotParams...)
+plot!(xCurve1(y), y, color=:blue, label="Continuation"; plotParams...)
 y = centery1:-0.01:0
 plot!(xCurve1(y), y, color=:blue, linestyle=:dot, label=""; plotParams...)
 
 # ODE curve
-plot!([p2, p2], [initialCondition, centery2-sqrt((p2-centerx2)/curve2)], label="Full Approach", color=:red; plotParams...)
+plot!([p2, p2], [initialCondition, centery2-sqrt((p2-centerx2)/curve2)], label="Full", color=:red, legend=:bottomright; plotParams...)
 
 # Initial Condition
-plotB = hline!([initialCondition], label="Initial Condition", color=:pink; plotParams...)
+plotB = hline!([initialCondition], label="IC", color=:pink; plotParams...)
 
-plot(plotA, plotB, layout=l, size=(900,300), dpi=300, margin=5Plots.mm, left_margin=10Plots.mm)
-annotate!([(-0.1, 5, text("A", 16, :black)), (1.13, 5, text("B", 16, :black))])
+plot(plotA, plotB, layout=l, size=(539,250), dpi=300, margin=5Plots.mm, left_margin=10Plots.mm)
+annotate!([(-0.1, 5, text("A", 12, :black)), (1.38, 5, text("B", 12, :black))])
 savefig("results/diagrams/possibleProblems.pdf")
 
 # Convergence figure
@@ -74,18 +74,17 @@ frequency = 790.0
 x = 0:0.0001:1
 y = @. 0.9*exp(-4.8x)+0.1+0.003*sin(frequency*x)
 plot(x, y, ylim=(0,1), xlim=(0,1), legend=false, yticks=nothing, xticks=nothing, xlabel="Time", 
-ylabel="Slow Variable", left_margin=5Plots.mm; dpi=plotParams.dpi, size=plotParams.size, linewidth=1.25)
+ylabel="Slow Variable", left_margin=5Plots.mm; dpi=plotParams.dpi, size=(255,250), linewidth=1.25)
 
-lens!([0.95, 1.0], [0.10, 0.125], inset = (1, bbox(0.5, 0.1, 0.4, 0.4)), xticks=nothing, 
+lens!([0.95, 1.0], [0.10, 0.125], inset = (1, bbox(0.4, 0.1, 0.5, 0.5)), xticks=nothing, 
 yticks=nothing, label="", title="Converged", titlefontcolor=:darkgreen, titlefontsize=10, 
 foreground_color_legend=nothing, framestyle=:box)
 # Alignment on the period isn't done in the actual convergence check, but we include many more oscillations than we can show in this figure
 period = 2*pi/frequency
 m = minimum(y[1-2*period.<=x.<=1.0])
-plot!([1-2*period, 1.0], [m, m], subplot=2, color=:purple, label="Range"; plotParams...)
+plot!([1-2*period, 1.0], [m, m], subplot=2, color=:purple, label=" Range"; plotParams...)
 m = maximum(y[1-2*period.<=x.<=1.0])
 plot!([1-2*period, 1.0], [m, m], subplot=2, color=:purple, label=""; plotParams...)
 m = mean(y[0.95.<=x.<=0.95+2*period])
-plot!([0.95, 0.95+2*period], [m, m], subplot=2, color=:red, label="Average"; plotParams...)
-
-savefig("results/diagrams/convergence.pdf")
+plot!([0.95, 0.95+2*period], [m, m], subplot=2, color=:red, label=" Average"; plotParams...)
+savefig("results/diagrams/convergenceDiagram.pdf")
