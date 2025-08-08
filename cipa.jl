0c06734619f369a0aeb5e1238fa2b181fea8c108
Matt J Owen <matt.owen@bristol.ac.uk>
2025-08-08 15:03:58 +0100

    added extra parameter to sim timing

--- cipa.jl
+++ cipa.jl
@@ -48,12 +48,12 @@ end
 
 setter! = setp(getsys(ml), [:IKb₊GKb_b, :INa₊GNa, :IKr₊GKr_b, :IK1₊GK1_b, :INaCa_i₊Gncx_b,
                             :INaL₊GNaL_b, :IKs₊GKs_b, :IpCa₊GpCa, :Ito₊Gto_b, :ICaL₊PCa_b,
-                            :INaK₊Pnak_b, :INab₊PNab, :ICab₊PCab])
+                            :INaK₊Pnak_b, :INab₊PNab, :ICab₊PCab, :IKr₊B41])
 
 function param_map!(prob, params)
     defaultParams = [0.003, 75.0, 0.04658545454545456, 0.3239783999999998, 0.0008,
                      0.019957499999999975, 0.006358000000000001, 0.0005, 0.02, 0.0001007,
-                     30.0, 3.75e-10, 2.5e-8]
+                     30.0, 3.75e-10, 2.5e-8, -0.02877]
     setter!(prob, defaultParams .* params)
     return nothing
 end
